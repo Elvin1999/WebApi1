552273fd9cdcb423463cc2ff4209da18d298e172
e.camalzade <e.camalzade@STHQ0118-01>
2022-04-30 16:34:46 +0400

    done

--- WebApi1/DataAccess/NorthwindContext.cs
+++ WebApi1/DataAccess/NorthwindContext.cs
@@ -9,7 +9,7 @@ namespace WebApi1.DataAccess
         {
             string conn = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             optionsBuilder.UseSqlServer(conn);
-        }
+        } 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
     }
